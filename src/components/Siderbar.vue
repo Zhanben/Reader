ffee526393cd3ec508bfeb82b7dff6108943259c
ben.zhang <ben.zhang@ucloud.cn>
2019-08-10 18:10:02 +0800

    first commit

---
+++ src/components/Siderbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <div class="card-header" align="center">
+      <img src="http://wx.qlogo.cn/mmhead/Q3auHgzwzM5VP8rbv4fBibDLRoibcezeC7aMx2qs4hfUWtw8Cp6PDZ7Q/0"
+           class="avatar img-circle img-responsive"/>
+      <p><strong> ben.zhang </strong></p>
+      <p class="card-title">1天前 阅读</p>
+    </div>
+    <div class="card-block">
+      <p>
+        <img src="http://wx.qlogo.cn/mmhead/Q3auHgzwzM5VP8rbv4fBibDLRoibcezeC7aMx2qs4hfUWtw8Cp6PDZ7Q/0"
+             class="align-self-center mr-2 mt-2 rounded-circle" style="width:30px" alt="test"/>
+        云计算前沿资讯
+        <span class="badge badge-pill badge-danger">12</span>
+      </p>
+      <p>
+        <img src="http://wx.qlogo.cn/mmhead/Q3auHgzwzM5VP8rbv4fBibDLRoibcezeC7aMx2qs4hfUWtw8Cp6PDZ7Q/0"
+             class="align-self-center mr-2 mt-2 rounded-circle" style="width:30px" alt="test"/>
+        科技每日推送
+        <span class="badge badge-pill badge-danger">3</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+  .avatar {
+    height: 75px;
+    margin: 0 auto;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+  .mpavatar {
+    height: 30px;
+    margin: 0 auto;
+    margin-top: 2px;
+    margin-bottom: 2px;
+  }
+
+  .img-circle {
+    border-radius: 50%;
+  }
+</style>
